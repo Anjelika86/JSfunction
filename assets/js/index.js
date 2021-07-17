c368640a5dffb19d2e4cf475a3647ec904606448
Anjelika <fominaangelika1986@gmail.com>
2021-07-17 22:44:42 +0300

    Add checkMultiplicity function

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,4 +4,13 @@ const isAdult = function (number) {
   }
   return number >= 18;
 };
-console.log(isAdult(20));
+console.log(isAdult(17));
+
+const checkMultiplicity = function (number1, number2) {
+  if (isNaN(number1 - number2)) {
+    return null;
+  }
+
+  return number1 % number2 === 0;
+};
+console.log(checkMultiplicity(15, 4));
