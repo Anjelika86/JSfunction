819bf87eab8ecbb9bc47d66497687a38a9e6e10f
Anjelika <fominaangelika1986@gmail.com>
2021-07-18 15:52:11 +0300

    Add checkTriangle function

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ const isAdult = function (number) {
   }
   return number >= 18;
 };
-console.log(isAdult(17));
+console.log(isAdult(18));
 
 const checkMultiplicity = function (number1, number2) {
   if (isNaN(number1 - number2)) {
@@ -13,4 +13,17 @@ const checkMultiplicity = function (number1, number2) {
 
   return number1 % number2 === 0;
 };
-console.log(checkMultiplicity(15, 4));
+console.log(checkMultiplicity(15, 5));
+
+const checkTriangle = function (a, b, c) {
+  if (isNaN(a - b - c)) {
+    return null;
+  }
+  if (a <= 0 || b <= 0 || c <= 0) {
+    return null;
+  }
+
+  return a < b + c && b < a + c && c < a + b;
+};
+
+console.log(checkTriangle(3, 3, 3));
